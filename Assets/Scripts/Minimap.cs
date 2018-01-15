180d5de2f5355b68ea8af0cfa834347eabbe01ff
John Wonson <jmwonson@hotmail.com>
2018-01-15 18:33:08 +1100

    Minimap using Render Textures

---
+++ Assets/Scripts/Minimap.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+
+namespace MinimapConfig
+{
+    public class Minimap : MonoBehaviour
+    {
+        public Transform player;
+
+        void LateUpdate ()
+        {
+            Vector3 newPosition = player.position;
+
+            newPosition.y = transform.position.y;
+
+            transform.position = newPosition;
+
+            transform.rotation = Quaternion.Euler(90f, player.eulerAngles.y, 0f);
+        }
+    }
+}
---
+++ Assets/Scripts/Movement.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+
+namespace MinimapConfig
+{
+    public class Movement : MonoBehaviour
+    {
+        public float speed = 10f;
+        public float rotationSpeed = 10f;
+
+        private Vector3 movement;
+
+        private float rotation;
+
+        // Update is called once per frame
+        void Update ()
+        {
+            movement.z = Input.GetAxis("Vertical") * speed * Time.deltaTime;
+
+            rotation = Input.GetAxis("Horizontal") * rotationSpeed * Time.deltaTime;
+        }
+
+        void FixedUpdate ()
+        {
+            transform.Translate(movement, Space.Self);
+
+            transform.Rotate(0f, rotation, 0f);
+        }
+    }
+}
